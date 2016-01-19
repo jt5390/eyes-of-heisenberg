1e903d306fa4b4b94d881ac7ace32894d7679676
Jessica Turner <jessicaturner@macbook-pro-6.wlan.dur.ac.uk>
2016-01-19 16:28:22 +0000

    more like the original code! seems to draw dispersion relation and gives zero value

--- dispersion.c
+++ dispersion.c
@@ -12,36 +12,35 @@
 #include <gsl/gsl_roots.h>
 #include <gsl/gsl_errno.h>
 #include "dispersion.h"
+#include <boost/math/constants/constants.hpp>
+#include <boost/multiprecision/cpp_dec_float.hpp>
 
 #define GW 2.0
 #define PI 3.14149
-//#define REL 0.000001//0.0001
-#define REL 1.0e-6
-#define ABS 0.0
-//#define ABS 0
+#define REL 1.0e-3
+#define ABS 1e-7
 #define DEBUG_MODE false
-//#define T_TRESHOLD 1e-10
-#define T_TRESHOLD 1e-10
+#define T_TRESHOLD 1e-12
 
 
 
+//Bose-distributions
 double nB(double p, double T){
-   double ans = 0.0;
-    if (p < 1e8*T){ ans = 1.0/(exp(p/T)-1.0);}
-    
-    return ans;
-}
-
-
-
-double nF(double p, double T, double M){
     double ans = 0.0;
-    //if (sqrt(p*p+M*M) < 1e5*T){ ans = 1.0/(exp(sqrt(p*p+M*M)/ T)+1.0);}
-    ans =1.0/(exp(sqrt(p*p+M*M)/T)+1.0); 
+    ans = 1.0/(exp(p/T)-1.0);
     return ans;
-
 }
 
+
+
+//Fermi-Dirac distribution
+    double nF(double p, double T, double M){
+        double ans = 0.0;
+        ans =1.0/(exp(sqrt(p*p+M*M)/T)+1.0);
+        return ans;
+    }
+
+//function which return -1 for positive nunber
 int whatsthatsign(double x){
 	if (x > 0) return 1;
 	if (x < 0) return -1;
@@ -52,124 +51,209 @@ return 0;
 
 
 double LpB(double p, double M, double k0, double k1){
+    double KK = k0*k0-k1*k1;
+    
+    double top1 = KK-M*M+2*p*(k0+k1);
+    double bot1 = KK-M*M+2*p*(k0-k1);
+    
+    double top2 = KK-M*M-2*p*(k0-k1);
+    double bot2 = KK-M*M-2*p*(k0+k1);
+    double arg1 = top1/bot1;
+    double arg2 = top2/bot2;
+    double arg3 = (top1*top2)/(bot1*bot2);
 
-	double top = pow(-k1*k1+k0*k0-M*M,2)+4*k1*(-k1*k1+k0*k0-M*M)*p - 4*(-k1*k1+k0*k0)*p*p;
-	double bot = pow(-k1*k1+k0*k0-M*M,2)-4*k1*(-k1*k1+k0*k0-M*M)*p - 4*(-k1*k1+k0*k0)*p*p;
-    if(top==0 || bot==0){std::cout<<"ERROR LpB: top: "<<top<<" bot: "<<bot<<" k0: "<<k0<<" k1: "<<k1<<std::endl;};
-	return gsl_sf_log_abs(top/bot);
+
+    
+//    if(arg1<0){std::cout<<"ERROR LpB: arg1 <0"<<std::endl;};
+//    if(arg1==0){std::cout<<"ERROR LpB: arg1 =0"<<std::endl;};
+//    if(arg2<0){std::cout<<"ERROR LpB: arg2 <0"<<std::endl;};
+//    if(arg2==0){std::cout<<"ERROR LpB: arg2 =0"<<std::endl;};
+    
+
+    double arg4= gsl_sf_log_abs(top1)-gsl_sf_log_abs(bot1)+gsl_sf_log_abs(top2)-gsl_sf_log_abs(bot2);
+    
+//   if(arg3==0|arg3<0){std::cout<<" LMB "<<" k1 "<<k1<<" k0 "<<k0<<" k0-k1 "<<k0-k1<<" p "<<p<<" top1 "<<top1<<" top2 "<<top2<<" bot1 "<<bot1<<" bot2 "<<bot2<<" arg3 "<<arg3<<" arg4 "<<arg4<<std::endl;};
+
+
+    return arg4;
+
+   
+
 }
+
+
+
+
 
 double LmB(double p, double M, double k0, double k1){
-	double top = pow(k0*k0-k1*k1-M*M,2)-4*pow(k0+k1,2)*p*p;
-	double bot = pow(k0*k0-k1*k1-M*M,2)-4*pow(k0-k1,2)*p*p;
-    if(top==0||bot==0){std::cout<<"ERROR LmB: top: "<<top<<" bot: "<<bot<<" k0: "<<k0<<" k1: "<<k1<<std::endl;};
-	return gsl_sf_log_abs(top/bot);
+    
+    double KK = k0*k0-k1*k1;
+    
+    double top1 = KK-M*M+2*p*(k0+k1);
+    double bot1 = KK-M*M+2*p*(k0-k1);
+    
+    double top2 = KK-M*M-2*p*(k0-k1);
+    double bot2 = KK-M*M-2*p*(k0+k1);
+    double arg1 = top1/bot1;
+    double arg2 = top2/bot2;
+    double arg3 = (top1*bot2)/(bot1*top2);
+    //if(arg3==0||arg3<0){std::cout<<"ERROR LmB: arg3 =0"<<" k1 "<<k1<<" k0 "<<k0<<" k0-k1 "<<k0-k1<<" p "<<p<<" M "<<M<<std::endl;};
+
+    
+    
+//    if(arg1<0){std::cout<<"ERROR LmB: arg1 <0"<<std::endl;};
+//    if(arg1==0){std::cout<<"ERROR LmB: arg1 =0"<<std::endl;};
+//    if(arg2<0){std::cout<<"ERROR LmB: arg2 <0"<<std::endl;};
+//    if(arg2==0){std::cout<<"ERROR LmB: arg2 =0"<<std::endl;};
+//
+    return gsl_sf_log_abs(top1)-gsl_sf_log_abs(bot1)-gsl_sf_log_abs(top2)+gsl_sf_log_abs(bot2);
+
+    //return gsl_sf_log_abs(top1*bot2/(bot1*top2));
+   
+//    if(top1==0||bot1==0){std::cout<<"ERROR LmB: top1: "<<top1<<" bot1: "<<bot1<<" k0: "<<k0<<" k1: "<<k1<<std::endl;};
+//    if(top2==0||bot2==0){std::cout<<"ERROR LmB: top2: "<<top2<<" bot2: "<<bot2<<" k0: "<<k0<<" k1: "<<k1<<std::endl;};
+//    
+
 }
+
 
 double LpF(double p, double M, double k0, double k1){
+    
+    double KK = k0*k0-k1*k1;
+    double E = sqrt(p*p+M*M);
+    
+    double top1 = KK+M*M+2*E*k0+2*p*k1;
+    double bot1 = KK+M*M+2*E*k0-2*p*k1;
+    
+    double top2 = KK+M*M-2*E*k0+2*p*k1;
+    double bot2 = KK+M*M-2*E*k0-2*p*k1;
+    double arg1 = top1/bot1;
+    double arg2 = top2/bot2;
+    double arg3 = (top1*top2)/(bot1*bot2);
+    return gsl_sf_log_abs(top1)-gsl_sf_log_abs(bot1)+gsl_sf_log_abs(top2)-gsl_sf_log_abs(bot2);
 
-        double top=pow(k0*k0-k1*k1+M*M,2)+4*k1*(-k1*k1+k0*k0+M*M)*p-4*((p*p+M*M)*k0*k0-p*p*k1*k1);
-        double bot=pow(k0*k0-k1*k1+M*M,2)-4*k1*(-k1*k1+k0*k0+M*M)*p+4*((p*p+M*M)*k0*k0-p*p*k1*k1);
-    if(top==0||bot==0){std::cout<<"ERROR LpF: top: "<<top<<" bot: "<<bot<<" k0: "<<k0<<" k1: "<<k1<<std::endl;};
-         return gsl_sf_log_abs(top/bot);
-
+    
 }
+
 
 double LmF(double p, double M, double k0, double k1){
     
-        double top=pow(k0*k0-k1*k1+M*M,2)-4*(sqrt(p*p+M*M)*k0+p*k1)*(sqrt(p*p+M*M)*k0+p*k1);
-        double bot=pow(k0*k0-k1*k1+M*M,2)-4*(sqrt(p*p+M*M)*k0-p*k1)*(sqrt(p*p+M*M)*k0-p*k1);
-    if(top==0||bot==0){std::cout<<"ERROR LmF: top: "<<top<<" bot: "<<bot<<" k0: "<<k0<<" k1: "<<k1<<std::endl;};
-	return gsl_sf_log_abs(top/bot);
+    double KK = k0*k0-k1*k1;
+    double E = sqrt(p*p+M*M);
+    
+    double top1 = KK+M*M+2*E*k0+2*p*k1;
+    double bot1 = KK+M*M+2*E*k0-2*p*k1;
+    
+    double top2 = KK+M*M-2*E*k0+2*p*k1;
+    double bot2 = KK+M*M-2*E*k0-2*p*k1;
+    double arg1 = top1/bot1;
+    double arg2 = top2/bot2;
+    double arg3 = (top1*bot2)/(bot1*top2);
+
+    
+    return gsl_sf_log_abs(top1)-gsl_sf_log_abs(bot1)-gsl_sf_log_abs(top2)+gsl_sf_log_abs(bot2);
+    
+ 
 }
 
 
-// why is void here?
+
+//integrand Tr(k.Sigma)
 double TRKS_integrand(double p, void *d){
 	struct dispParams * params = (struct dispParams *)d;
 	double M = params->M;
 	double k1 = params->k1;
 	double k0 = params->k0;
 	double T = params->T;
-
-	//return nB(p,T)/(8*PI*PI)*p*(4+(M*M-k1*k1+k0*k0)*LpB(p,M,k0,k1)/(2*p*k1))+ (nF(p,T,M)*p*p)/(2*2*PI*2*PI*sqrt(p*p+M*M))*(4-(M*M-k1*k1+k0*k0)*LpF(p,M,k0,k1)/(2*p*k1));
-    return 1/(8*PI*PI)*(p*nB(p,T)*(4+((k0*k0-k1*k1+M*M)/(2*p*k1))*LpB(p,M,k0,k1))+  ((p*p*nF(p,T,M))/(p*p+M*M))*(4-((k0*k0-k1*k1+M*M)/(2*p*k1))*LmF(p,M,k0,k1)));
+    double f1=(4+((M*M-k1*k1+k0*k0)/(2*p*k1))*LpB(p,M,k0,k1))*nB(p,T)/p;
+    
+    double f2=(4-((M*M-k1*k1+k0*k0)/(2*p*k1))*LpF(p,M,k0,k1))*nF(p,T,M)/sqrt(p*p+M*M);
+    
+    
+    return (1/(8*PI*PI))*p*p*(f1+f2);
+    
 }
 
 
+
+
+// integrating Tr(k.Sigma)
 double TRKS(double T, double M,double k0, double k1)
 {
-
-	gsl_integration_workspace * w = gsl_integration_workspace_alloc (5000);
-	struct dispParams params = {T,M,k0,k1};
-    double result,error;
-	gsl_function F;
-	/*
-	 *Base is the generic starting value for what I base the integral size off.
-	 * Then this increases temperatyre in steps of "T" until I have reached a value below that the predefined threshold, T_TRESH
-	 * It then integrates this from 0 to value of T where cross the T_Treshold.
-	 */
-	double base = TRKS_integrand(T,&params);
-	double iT = 2;
-
-	for(iT=2;fabs(TRKS_integrand(iT*T,&params)/base)>=T_TRESHOLD;iT=iT+1)
-	{
-        //if(DEBUG_MODE){std::cout<<std::setprecision(9)<<" # TRKS integral base "<<base<<" iT: "<<iT<<" rat:  "<<fabs(TRKS_integrand(iT*T,&params)/base)<<std::endl;}
-	}
-
-
-	F.function = &TRKS_integrand;
-	F.params = &params;
-	
-	gsl_integration_qags(&F,0.0, iT*T, ABS, REL, 5000, w, &result, &error);
-	gsl_integration_workspace_free (w);
     
-
-	return result;
-   
-
+    gsl_integration_workspace * w = gsl_integration_workspace_alloc (50000);
+    struct dispParams params = {T,M,k0,k1};
+    double result,error;
+    gsl_function F;
+    /*
+     *Base is the generic starting value for what I base the integral size off.
+     * Then this increases temperatyre in steps of "T" until I have reached a value below that the predefined threshold, T_TRESH
+     * It then integrates this from 0 to value of T where cross the T_Treshold.
+     */
+    double base = TRKS_integrand(T,&params);
+    double iT = 2;
+    
+    for(iT=2;fabs(TRKS_integrand(iT*T,&params)/base)>=T_TRESHOLD;iT=iT+1)
+    {
+        if(DEBUG_MODE){std::cout<<std::setprecision(9)<<" # TRKS integral base "<<base<<" iT: "<<iT<<" rat:  "<<fabs(TRKS_integrand(iT*T,&params)/base)<<std::endl;}
+    }
+    
+    
+    F.function = &TRKS_integrand;
+    F.params = &params;
+    
+    gsl_integration_qags(&F,0.0, iT*T, ABS, REL, 50000, w, &result, &error);
+    gsl_integration_workspace_free (w);
+    
+    
+    return result;
+    
+    
 }
 
 
+//integrand Tr(u.Sigma)
 double TRUS_integrand(double p, void *d){
-	struct dispParams * params = (struct dispParams *)d;
-	double M = params->M;
-	double k1 = params->k1;
-	double k0 = params->k0;
-	double T = params->T;
-
-	//return nB(p,T)/(2*2*PI*2*PI*k1)*(p*LmB(p,M,k0,k1)+k0*LpB(p,M,k0,k1));
-    //return 1/(2*2*PI*2*PI)*(  p*LmB(p,M,k0,k1)*nB(p,T)/k1 + k0*LpB(p,M,k0,k1)*nB(p,T)/k1 + p*nF(p,T,M)*LmF(p,M,k0,k1)/k1 );
-    return 1/(8*PI*PI)*(     (p*nB(p,T)/k1)* (LmB(p,M,k1,k0)+(k0/p)*LpB(p,M,k1,k0) ) +( p*nF(p,T,M)/k1)*( LmF(p,M,k0,k1)) );
+    struct dispParams * params = (struct dispParams *)d;
+    double M = params->M;
+    double k1 = params->k1;
+    double k0 = params->k0;
+    double T = params->T;
+    double f1 = (LmB(p,M,k0,k1)+(k0/p)*LpB(p,M,k0,k1))*nB(p,T);
+    double f2 = nF(p,T,M)*LmF(p,M,k0,k1);
+    
+    return 1/(8*PI*PI)*(p/k1)*(f1+f2);
 }
 
 
+// integrating Tr(u.Sigma)
 double TRUS(double T, double M,double k0, double k1)
 {
-    //disParams getting defined again, can it be defined globally?
     
-	gsl_integration_workspace * w = gsl_integration_workspace_alloc (5000);
-	struct dispParams params = {T,M,k0,k1};
-	double result,error;
-	gsl_function F;
-	F.function = &TRUS_integrand;
-	F.params = &params;
-	
-	double base = TRUS_integrand(T,&params);
-	double iT = 2;
-
-	for(iT=2;fabs(TRUS_integrand(iT*T,&params)/base)>=T_TRESHOLD;iT=iT+1)
-	  {
-	 // if(DEBUG_MODE){	std::cout<<std::setprecision(9)<<"# TRKU integral base "<<base<<" iT: "<<iT<<" rat:  "<<fabs(TRUS_integrand(iT*T,&params)/base)<<std::endl;}
-		}
+    
+    gsl_integration_workspace * w = gsl_integration_workspace_alloc (50000);
+    struct dispParams params = {T,M,k0,k1};
+    double result,error;
+    gsl_function F;
+    F.function = &TRUS_integrand;
+    F.params = &params;
+    
+    double base = TRUS_integrand(T,&params);
+    double iT = 2;
+    
+    for(iT=2;fabs(TRUS_integrand(iT*T,&params)/base)>=T_TRESHOLD;iT=iT+1)
+    {
+        if(DEBUG_MODE){	std::cout<<std::setprecision(9)<<"# TRKU integral base "<<base<<" iT: "<<iT<<" rat:  "<<fabs(TRUS_integrand(iT*T,&params)/base)<<std::endl;}
+    }
     //std::cout << "now: " << iT << std::endl;
-	gsl_integration_qags(&F,0.0, iT*T, ABS, REL, 5000, w, &result, &error);
-	gsl_integration_workspace_free (w);
-
-	return result;
-
+    gsl_integration_qags(&F,0.0, iT*T, ABS, REL, 50000, w, &result, &error);
+    
+    
+    gsl_integration_workspace_free (w);
+    
+    return result;
+    
 }
-
 double TRS_integrand(double p, void*d){
 	
 	struct dispParams * params = (struct dispParams *)d;
@@ -188,7 +272,7 @@ double TRS(double T,double M,double k0,double k1)
 {
 
     
-	gsl_integration_workspace * w = gsl_integration_workspace_alloc (5000);
+	gsl_integration_workspace * w = gsl_integration_workspace_alloc (50000);
 	struct dispParams params = {T,M,k0,k1};
 	double result,error;
 	gsl_function F;
@@ -203,7 +287,7 @@ double TRS(double T,double M,double k0,double k1)
 	 // if(DEBUG_MODE){	std::cout<<std::setprecision(9)<<"# TRKU integral base "<<base<<" iT: "<<iT<<" rat:  "<<fabs(TRUS_integrand(iT*T,&params)/base)<<std::endl;}
 		}
     //std::cout << "now: " << iT << std::endl;
-	gsl_integration_qags(&F,0.0, iT*T, ABS, REL, 5000, w, &result, &error);
+	gsl_integration_qags(&F,0.0, iT*T, ABS, REL, 50000, w, &result, &error);
 	gsl_integration_workspace_free (w);
 
 	return result;
@@ -213,7 +297,9 @@ double TRS(double T,double M,double k0,double k1)
 
 
 double sigmaA(double T, double M, double k0, double k1){
-	return 1/(k1*k1)*(TRKS(T,M,k0,k1)-k0*TRUS(T,M,k0,k1));
+    
+    
+    	return (1/(k1*k1))*(TRKS(T,M,k0,k1)-k0*TRUS(T,M,k0,k1));
 }
 
 
@@ -230,16 +316,11 @@ double dispEquation(double k0, void * d){
 	struct dispParams2 * params = (struct dispParams2 *)d;
 	double T = params->T;
 	double M = params->M;
-        double k1 = params->k1;
+    double k1 = params->k1;
     
-    
-        //if(DEBUG_MODE){std::cout<<"sigma"<<" "<<(1+sigmaA(*params))*k0+sigmaB(*params)+(1+sigmaA(*params))*w1<<std::endl;}
-         // double ans_pos = (1+sigmaA(*params))*w0+sigmaB(*params)+(1+sigmaA(*params))*k1;
-         // double ans_neg = (1+sigmaA(T,M,k0,k1))*k0+sigmaB(T,M,k0,k1)-(1+sigmaA(T,M,k0,k1))*k1;
-	  double ans_mass= (1+sigmaA(T,M,k0,k1))*k0+sigmaB(T,M,k0,k1)-sqrt(pow(k1*(1.0+sigmaA(T,M,k0,k1)),2)+pow(M*(1-sigmaC(T,M,k0,k1)),2) );
-   	 return ans_mass;
-   // return ans_pos;
-
+    double ans_mass= (1+sigmaA(T,M,k0,k1))*k0+sigmaB(T,M,k0,k1)-k1*(1+sigmaA(T,M,k0,k1));
+    return ans_mass;
+  
 }
 
 double dispSolved(double T, double M, double k1){
@@ -254,7 +335,7 @@ double dispSolved(double T, double M, double k1){
 	gsl_root_fsolver * s     = gsl_root_fsolver_alloc (S);
     struct dispParams2 p = {T,M,k1};
 
-	//This for loop will start a bit to the left and roight of input k0 and increas:e the bracket until we get opposite signs
+	//This for loop will start a bit to the left and right of input k0 and increas:e the bracket until we get opposite signs
 	//Will only fail if its all negative or positive, which can happen at very low k/T (presumably due to not complete expressions)
 	
 	double x_lo=k1;
@@ -262,13 +343,13 @@ double dispSolved(double T, double M, double k1){
 	
 
 	double m = 1e-8;
-    for(m = 1e-6; whatsthatsign(dispEquation(k1*pow(10,m),&p))==whatsthatsign(dispEquation(k1*pow(10,-m),&p)); m=m+0.005)
+    for(m = 1e-5; whatsthatsign(dispEquation(k1*pow(10,m),&p))==whatsthatsign(dispEquation(k1*pow(10,-m),&p)); m=m+0.005)
    	{
         if(DEBUG_MODE){std::cout<<m<<" "<<pow(10,m)<<" "<<whatsthatsign(dispEquation(k1*pow(10,m),&p))<<" "<<whatsthatsign(dispEquation(k1*pow(10,-m),&p))<<std::endl;}
         if(DEBUG_MODE){std::cout<<m<<" "<<pow(10,m)<<" "<<dispEquation(k1*pow(10,m),&p)<<" "<<-m<<" "<<pow(10,-m)<<" "<<dispEquation(k1*pow(10,-m),&p)<<std::endl;}
 		if(m>4)
 		{
-			std::cout<<"# ERROR: dispSolved@'dispersion.c' rootfinder: Wandered from k0, didnt bracket the root. "<<std::endl;
+			//std::cout<<"# ERROR: dispSolved@'dispersion.c' rootfinder: Wandered from k0, didnt bracket the root. "<<std::endl;
 		}
 	
 	}
--- lepto.cxx
+++ lepto.cxx
@@ -8,17 +8,26 @@
 #include <unistd.h>
 #include <gsl/gsl_rng.h>
 #include <gsl/gsl_randist.h>
+#include <boost/math/constants/constants.hpp>
+#include <boost/multiprecision/cpp_dec_float.hpp>
+
 
 #include "epsilon.h"
 #include "washout.h"
 #include "dispersion.h"
 
+using boost::multiprecision::cpp_dec_float_50;
+
+
+
+
 int main(int argc, char * argv[])
 {
 
+    
 double cutoff = 0.0;
     double M = 1e13;
-bool boolMe = false;
+bool boolMe = true;
 
 	int c;
 	while ((c = getopt (argc, argv, "WC:M:")) != -1)
@@ -48,33 +57,49 @@ bool boolMe = false;
  	}}
 
 
-	//std::cout<<"#Cutoff: "<<cutoff<<std::endl;
-	double baseT = 1e15;
+	
+	double baseT = 1;
 
 if(boolMe){
-     //T,M,k1,k0
-	
 
-	//for(double x = -2; x<1; x=x+0.0033)
-    for(double x = 0.6;x >0.04 ; x=x-0.01)
-	{
-        double k1 = x*baseT;
-        // dispSolved now takes (T,M,k1) simple.
-        double k0_09 = dispSolved(baseT, 0.9*baseT, k1);
-        std::cout<<fabs(k1)/baseT<<" "<<(k0_09-fabs(k1))/baseT<<" "<<x<<std::endl;
-	}
+   // for(double x = 1.0;x >0.01 ; x=x-0.001)
+    //for(double x = 1.0;x >0.01 ; x=x-0.001)
+    for(double x = 0.85;x >-0.3 ; x=x-0.01)
+    {
+        //double k1 =x*baseT;
+        double k1=0.1*baseT;
+        double k0=x*baseT;
+        // double dispEquation(k0,&q);
+
+        std::cout<<k0<<" "<<(1+sigmaA(baseT,1.5*baseT,k1,k0)*k0+sigmaB(baseT,1.5*baseT,k0,k1)-k1*(1+sigmaA(baseT,1.5*baseT,k0,k1)))<<" "<<std::endl;
+       
+       // double k0_09 = dispSolved(baseT, 1.5*baseT, k1);
+       // double sigA = sigmaA(baseT,  M, k0_09, k1);
+        //double sigB = sigmaB(baseT,  M, k0_09, k1);
+//        double disp = (1+sigA)*k0_09+sigB-(1+sigA)*k1;
+      //  std::cout<<sigA<<" "<<sigB<<" "<<disp<<std::endl;
+       
+       // std::cout<<(fabs(k1))/(baseT)<<" "<<(k0_09-fabs(k1))/(2*baseT)<<std::endl;
+ 
+
+        //std::cout<<(fabs(k1))/(baseT)<<std::endl;
+    }
+
+    
+    
 
 
-} else {
+}
+else {
 
 	//for(double z = -2; z<1; z=z+0.05){
-	//	std::cout<<pow(10,z)<<" "<<epsilon_eff_ratio(M/(pow(10,z)),M,cutoff)-1.0<<std::endl;
+	//	std::cout<<pow(10,z)<<" "<<epsilon_eff_ratio(M/(pow(10,z)),M,cutoff)-1.2<<std::endl;
 	//}
-    double ik0 = 1.0;
+    double ik0 = 1.2;
     baseT=1e14;
-    struct dispParams2 mypar ={baseT,0.9*baseT,0.4*baseT};
-    for(ik0=-2.0; ik0<0.0;ik0=ik0+0.01){
-        std::cout<<pow(10,ik0)<<"  "<<dispEquation(pow(10,ik0)*baseT,&mypar)/baseT<<std::endl;
+    struct dispParams2 mypar ={baseT,1.0*baseT,0.4*baseT};
+    for(ik0=-0.9; ik0<0.0;ik0=ik0+0.01){
+        std::cout<<pow(10,ik0)<<" wrong "<<dispEquation(pow(10,ik0)*baseT,&mypar)/baseT<<std::endl;
         
         }
     }
